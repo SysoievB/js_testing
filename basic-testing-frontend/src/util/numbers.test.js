a672314ab41a454e9f146494501071b0efe0e775
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-06-16 09:12:05 +0200

    Added few unit tests

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,46 +1,48 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 import {transformToNumber} from "./numbers";
 
-it('should return number from string', () => {
-    //Arrange
-    const inputValue = '23';
+describe('transformToNumber()', () => {
+    it('should return number from string', () => {
+        //Arrange
+        const inputValue = '23';
 
-    //Act
-    const result = transformToNumber(inputValue);
+        //Act
+        const result = transformToNumber(inputValue);
 
-    //Assert
-    expect(result).toBe(23);
-});
+        //Assert
+        expect(result).toBe(23);
+    });
 
-it('should return number from number', () => {
-    //Arrange
-    const inputValue = 23;
+    it('should return number from number', () => {
+        //Arrange
+        const inputValue = 23;
 
-    //Act
-    const result = transformToNumber(inputValue);
+        //Act
+        const result = transformToNumber(inputValue);
 
-    //Assert
-    expect(result).toBe(23);
-});
+        //Assert
+        expect(result).toBe(23);
+    });
 
-it('should yield NaN if string is not a number', () => {
-    //Arrange
-    const inputValue = 'hello';
+    it('should yield NaN if string is not a number', () => {
+        //Arrange
+        const inputValue = 'hello';
 
-    //Act
-    const result = transformToNumber(inputValue);
+        //Act
+        const result = transformToNumber(inputValue);
 
-    //Assert
-    expect(result).toBeNaN();
-});
+        //Assert
+        expect(result).toBeNaN();
+    });
 
-it('should return 0 from null', () => {
-    //Arrange
-    const inputValue = null;
+    it('should return 0 from null', () => {
+        //Arrange
+        const inputValue = null;
 
-    //Act
-    const result = transformToNumber(inputValue);
+        //Act
+        const result = transformToNumber(inputValue);
 
-    //Assert
-    expect(result).toBe(0);
-});
+        //Assert
+        expect(result).toBe(0);
+    });
+})
